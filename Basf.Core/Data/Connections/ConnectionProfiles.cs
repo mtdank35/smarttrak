b4d3e1a8ce104c4183b6c38c5a4a81a3f114ef3e
David <David>
2016-06-03 12:54:31 -0400

    [Basf.Core] strip connection profile init

--- Basf.Core/Data/Connections/ConnectionProfiles.cs
+++ Basf.Core/Data/Connections/ConnectionProfiles.cs
@@ -17,21 +17,9 @@ namespace Basf
         // FUTURE: Get List
         // FUTURE: Delete
 
-        public static string DefThing()
-        {
-            var db1 = "Data Source=.\\SQL_ICCM;Initial Catalog=CustDataNA;Integrated Security=False;User ID=sa;Password=rUnt94thigh=kAnE~Lover97Lid;Connect Timeout=5";
-            var db2 = "Data Source=.\\SQL_ICCM;Initial Catalog=iccm_db;Integrated Security=False;User ID=sa;Password=rUnt94thigh=kAnE~Lover97Lid;Connect Timeout=5";
-            var cp = new ConnectionProfile("DEFAULT", db1, db2);
-            return JsonConvert.SerializeObject(cp, Formatting.Indented);
-        }
-
-        public static ConnectionProfile GetThing(string dbcs)
-        {
-            return JsonConvert.DeserializeObject<ConnectionProfile>(dbcs);
-        }
-
         public static ConnectionProfile Get(string name)
         {
+            // FUTURE: read this from disk?
             // grab profile data from embedded resource
             var assembly = Assembly.GetExecutingAssembly();
             var resourceName = "Basf.Core.Data.Connections.DEFAULT-CP.json";
