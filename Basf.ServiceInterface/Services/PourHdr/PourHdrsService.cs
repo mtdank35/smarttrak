0726db9f94f2468e86db2d6ae220ee85a84fe255
David <David>
2016-07-05 16:21:36 -0400

    [BasfSvr] add api to fetch all pourhdr records from custdata* db

---
+++ Basf.ServiceInterface/Services/PourHdr/PourHdrsService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceStack;
+using System.Data.SqlClient;
+using BasfSvr.ServiceModel;
+using Humanizer;
+
+namespace BasfSvr.ServiceInterface
+{
+    public class PourHdrsService : Service
+    {
+        public DbContext DbContext { get; set; }
+
+        public object Get(PourHdrRequest request)
+        {
+            using (var dbi = DbContext.NewCustDbInstance())
+            {
+                var things = dbi.PourHdr.GetList(null);
+                var response = new PourHdrResponse();
+                response.PourHdrs = things.ToList();
+                return response;
+            }
+        }
+    }
+}
---
+++ BasfSvr.ServiceModel/Models/PourHdr/PourHdrRequestResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ServiceStack;
+using Basf.Data.Tables;
+using System.Collections.Generic;
+
+namespace BasfSvr.ServiceModel
+{
+    [Route("/pourhdrs", "GET")]
+    public class PourHdrRequest : BaseRequest
+    {
+    }
+
+    public class PourHdrResponse : BaseResponse
+    {
+        public List<PourHdr> PourHdrs { get; set; }
+    }
+}
