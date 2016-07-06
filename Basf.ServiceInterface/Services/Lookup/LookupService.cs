e473f3cc5d549307bf4848f74a3dfb892fa23db6
David <David>
2016-07-06 09:58:21 -0400

    [BasfSvr] add 'lookup" request/response with service implementation for a generic lookup implementation, allow lookup of pntline0 data

---
+++ Basf.ServiceInterface/Services/Lookup/LookupService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceStack;
+using System.Data.SqlClient;
+using DapperExtensions;
+using Humanizer;
+using Basf.Data.Tables;
+using BasfSvr.ServiceModel;
+using BasfSvr.ServiceModel.Exceptions;
+
+namespace BasfSvr.ServiceInterface
+{
+    public class LookupService : Service
+    {
+        public DbContext DbContext { get; set; }
+
+        public object Get(LookupRequest request)
+        {
+            using (var dbi = DbContext.NewDataDbInstance())
+            {
+                var response = new LookupResponse();
+                switch (request.LookupType.ToUpperInvariant())
+                {
+                    case "PNTLINE":
+                        // limit to custavai=1
+                        var pred = Predicates.Field<PntLine>(x => x.custavai, Operator.Eq, true);
+                        var lines = dbi.PntLine.GetList(pred);
+                        if (lines.Count > 0)
+                        {
+                            response.Values = new List<Lookup>();
+                            foreach (var line in lines)
+                                response.Values.Add(new Lookup() { DbVal = line.paintline, DisplayVal = line.linename });
+                        }
+                        break;
+                    default:
+                        throw new HttpError(System.Net.HttpStatusCode.NotAcceptable, "INVALID_LOOKUP_TYPE",
+                            String.Format("LookupType '{0}' not valid.", request.LookupType));
+                }
+                return response;
+            }
+        }
+    }
+}
---
+++ BasfSvr.ServiceModel/Models/Lookups/LookupRequestResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using ServiceStack;
+using Basf.Data.Tables;
+using System.Collections.Generic;
+
+namespace BasfSvr.ServiceModel
+{
+    [Route("/lookup/{LookupType}", "GET")]
+    public class LookupRequest : BaseRequest
+    {
+        public string LookupType { get; set; }
+    }
+
+    public class LookupResponse : BaseResponse
+    {
+        public List<Lookup> Values { get; set; }
+    }
+
+    public class Lookup
+    {
+        public string DbVal { get; set; }
+        public string DisplayVal { get; set; }
+    }
+}
